341079fd763e52bddbda1f59301ea92b651a6c25
June <yoo.junehyuk@gmail.com>
2026-03-28 01:08:28 -0500

    Updating maps visuals

--- frontend/src/components/map/CampusMap.tsx
+++ frontend/src/components/map/CampusMap.tsx
@@ -21,16 +21,25 @@ function CampusMap({
   submissions,
   onMapClick,
 }: CampusMapProps) {
+  const maxBounds: LatLngTuple[] = [
+    [center["0"] - 0.02, center["1"] - 0.02],
+    [center["0"] + 0.02, center["1"] + 0.02],
+  ];
   return (
     <MapContainer
       center={center}
       zoom={16}
       style={{ height: "100%", width: "100%" }}
       scrollWheelZoom
+      minZoom={14}
+      maxZoom={18}
+      maxBounds={maxBounds}
     >
       <TileLayer
-        attribution="&copy; OpenStreetMap contributors"
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        // attribution="&copy; OpenStreetMap contributors"
+        // url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://stadiamaps.com/" target="_blank">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>'
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
       />
 
       <MapClickHandler onSelect={onMapClick} />
