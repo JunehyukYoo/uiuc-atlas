e6274be353bc8c46252b06a0fecd05e54ab05f5b
June <yoo.junehyuk@gmail.com>
2026-05-01 13:56:58 -0500

    feat: Added favicon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import favicon from "/favicon.svg";
 
 function Navbar() {
   const navigate = useNavigate();
   return (
     <nav className="h-16 shrink-0 p-4 flex justify-between items-center border-b-4 border-accent-foreground">
       <h1
-        className="text-2xl hover:cursor-pointer"
+        className="text-2xl hover:cursor-pointer flex items-center gap-2"
         onClick={() => navigate("/")}
       >
+        <img src={favicon} className="w-10" />
         UIUC Atlas
       </h1>
 
